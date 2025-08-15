68e66bc8a8729b5fb9cb46f5b6077a31e72e74aa
spamforflo-arch <spamforflo@gmail.com>
2025-08-15 23:34:18 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE = 'quest-ui-cache-v1';
+const ASSETS = ['./','./index.html','./manifest.json','./service-worker.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
